ff6b1b1a6442188f911442b9f71cc323e3a73834
roxygrid77 <roycecrraphael@gmail.com>
2025-07-22 23:08:34 +0530

    Update p2.c

--- p2.c
+++ p2.c
@@ -1,14 +1,12 @@
 //Checking even or odd
 #include <stdio.h>
 int main(){
-    int num;
-    printf("Enter a number:\n");
-    scanf("%d",&num);
-
-    if(num%2 == 0){
-        printf("Even");
-    }else{
-        printf("Odd");
-    }
+    int num1,num2,sum;
+    printf("Enter first number:\n");
+    scanf("%d",&num1);
+    printf("Enter second number:\n");
+    scanf("%d",&num2);
+    sum = num1+num2;
+    printf("%d\n",&sum);
     return 0;
 }
