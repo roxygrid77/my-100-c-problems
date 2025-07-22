043959161f76ae512718afd8f1bad54710132806
roxygrid77 <roycecrraphael@gmail.com>
2025-07-22 23:40:35 +0530

    Add files via upload

---
+++ p4.c
@@ -0,0 +1,13 @@
+//area of rectangle
+#include <stdio.h>
+int main(){
+    int breadth,width,area;
+    printf("enter the breadth :");
+    scanf("%d",&breadth);
+    printf("enter the width:");
+    scanf("%d",&width);
+    area = breadth*width;
+    printf("area of rectangle:%d\n",area);
+    return 0;
+
+}
