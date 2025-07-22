7262836cd7fb0c4c2fb37ddd1a1537667d728b98
roxygrid77 <roycecrraphael@gmail.com>
2025-07-22 22:59:22 +0530

    Update p1.c

--- p1.c
+++ p1.c
@@ -3,14 +3,6 @@
  
 
 int main(){
-    int n = 55;
-    if(n>0){
-        printf("The number is positive");
-    }else if(n<0){
-        printf("The number is negative");
-    }else{
-        printf("Zero");
-    }
-
+    printf("Namah Shivaya!");
     return 0;
 }
