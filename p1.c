4a56e38176f703e43cbb7c174bdd8f7fc8cfd6db
roxygrid77 <roycecrraphael@gmail.com>
2025-07-22 22:06:19 +0530

    Add files via upload

---
+++ p1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+
+int main(){
+    int n = 55;
+    if(n>0){
+        printf("The number is positive");
+    }else if(n<0){
+        printf("The number is negative");
+    }else{
+        printf("Zero");
+    }
+
+    return 0;
+}
