0765614e7ea12c9b304960aeb4e16a6e01a7fd06
roxygrid77 <roycecrraphael@gmail.com>
2025-07-22 23:27:54 +0530

    Add files via upload

---
+++ p3.c
@@ -0,0 +1,11 @@
+
+//printing namahshivaya with name
+#include <stdio.h>
+
+int main(){
+    char name [20];
+    printf("enter name:");
+    scanf("%s",name);
+    printf("Namahshivaya!!!%s",name);
+    return 0;
+}
