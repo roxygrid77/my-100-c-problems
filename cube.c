4ed05569f25a100442d4bee1258657066bdc70be
roxygrid77 <roycecrraphael@gmail.com>
2025-07-25 19:22:50 +0530

    Add files via upload

---
+++ cube.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+int main(){
+    int num1,cube;
+    printf("enter the  number:");
+    scanf("%d",&num1);
+    
+    cube = num1*num1*num1;
+    printf("%d\n",cube);
+    
+    return 0;
+}
