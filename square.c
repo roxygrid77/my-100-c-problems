ab638d56a6b7eae391d95df547eb5572c14f29a5
roxygrid77 <roycecrraphael@gmail.com>
2025-07-25 19:18:27 +0530

    Add files via upload

---
+++ square.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+int main(){
+    int num1,square;
+    printf("enter the  number:");
+    scanf("%d",&num1);
+    
+    square = num1 * num1;
+    printf("%d\n",square);
+    
+    return 0;
+}
