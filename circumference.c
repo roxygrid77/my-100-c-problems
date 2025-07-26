1b6f9939dc720011c3a9a821ea1f5251f4c324c9
roxygrid77 <roycecrraphael@gmail.com>
2025-07-26 19:41:11 +0530

    Add files via upload

---
+++ circumference.c
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+#include <math.h>
+#define PI 3.142
+void main(){
+    float radius = 5,circumference;
+    circumference = 2 * PI * radius;
+    printf("circumference of circle = %5.2f\n",circumference);
+}
